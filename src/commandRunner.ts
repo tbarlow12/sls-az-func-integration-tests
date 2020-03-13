868a2c8c22af643fa5157741df95b7d717fdbc3d
Tanner Barlow <tanner.barlow.dev@gmail.com>
2020-03-12 21:17:07 -0700

    WIP

--- src/commandRunner.ts
+++ src/commandRunner.ts
@@ -1,71 +1,45 @@
-const { spawn } = require("child_process")
-const { readdirSync } = require("fs");
+import { spawn } from "child_process"
 
-// const tests = getDirectories();
-
-const commands = [
-  ["npm link serverless-azure-functions", []],
-  ["sls offline build", []],
-  ["ls hello", []],
-  ["sls offline cleanup", []],
-  // "sls deploy",
-  // "sls invoke -f hello -d '{\"name\":\"Azure\"}'",
-  // "sls remove"
-]
-
-const tests = [ "node10-linux" ];
-
-for (const test of tests) {
-  runCommands(test, commands);
+export interface Validation {
+  command: string;
+  expectedStdout: string[];
 }
 
-function runCommands(testName, commands) {
-  if (commands.length === 0) {
+export function runCommands(directory: string, validations: Validation[]) {
+  if (validations.length === 0) {
     return;
   }
-  const command = commands[0];
-  const split = command.split(" ");
-  // sls
+  const validation = validations[0];
+  const split = validation.command.split(" ");
+
   const commandName = getCommandName(split[0]);
-  // deploy
   const subCommandName = split[1];
-  
-  // ["deploy"]
+
   const args = split.slice(1, split.length);
 
   const spawned = createSpawn(
-    testName,
+    directory,
     commandName,
     args,
     (stdout, stderr) => {
       // TODO Evaluate and store results for pass
-      console.log(`'${testName}' - ${commandName} passed`)
-      runCommands(testName, commands.slice(1, commands.length));
+      console.log(`'${directory}' - ${commandName} passed`)
+      runCommands(directory, validations.slice(1, validations.length));
     },
     (stdout, stderr) => {
       // TODO Evaluate and store results for failure
-      console.log(`'${testName}' - ${commandName} failed`)
+      console.log(`'${directory}' - ${commandName} failed`)
     }
   )
 }
 
-function slsTest(testName, args, onSuccess) {
-  console.log(`Running ${args.join(" ")} test for ${testName}`);
-  const command = createSpawn(
-    testName,
-    sls,
-    args,
-    (stdout, stderr) => {
-      // TODO evaluate output for command
-      onSuccess();
-    },
-    (stdout, stderr) => {
-      // TODO record error for deploy failure
-    }
-  )
-}
+function createSpawn(
+  cwd: string,
+  command: string,
+  args: string[],
+  onPass: (stdout: string, stderr: string) => void,
+  onFail: (stdout: string, stderr: string) => void) {
 
-function createSpawn(cwd, command, args, onPass, onFail) {
   console.log(`Spawning command "${command} ${args.join(" ")}" in directory ${cwd}`)
   const childProcess = spawn(command, args, {
     env: process.env,
@@ -102,17 +76,10 @@ function createSpawn(cwd, command, args, onPass, onFail) {
   return childProcess
 }
 
-
 function getCommandName(command) {
   const executables = new Set(["sls", "npm"])
   if (executables.has(command) && process.platform === "win32") {
     return command + ".cmd";
   }
   return command;
-}
-
-function getDirectories(source = ".") {
-  return readdirSync(source, { withFileTypes: true })
-    .filter(dirent => dirent.isDirectory())
-    .map(dirent => dirent.name)
 }
